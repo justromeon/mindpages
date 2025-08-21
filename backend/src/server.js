92a0acb1b9918fedc6c607f56ead3d8ffa607b7c
Justin Kian Romeon <romeonjustin@gmail.com>
2025-08-21 16:54:31 +0800

    feat: setup mongodb and create Note model + schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import notesRoutes from './routes/notesRoutes.js'
+import { connectDB } from './config/db.js'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+connectDB()
 
 app.use('/api/notes', notesRoutes)
 
