16f75bba7a329d59a34f61d5be1df5b403736f92
Justin Kian Romeon <romeonjustin@gmail.com>
2025-08-26 19:37:07 +0800

    feat: integrate backend and frontend with cors and axios

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js'
 import dotenv from 'dotenv'
 import ratelimiter from './middleware/rateLimiter.js'
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
@@ -11,6 +12,9 @@ const port = process.env.PORT || 3000
 connectDB()
 
 //Middleware
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 app.use(express.json())
 app.use(ratelimiter)
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react"
 import NavBar from "../components/NavBar"
+import RateLimited from "../components/RateLimited"
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 const Home = () => {
-  return <>
+  const [isRateLimited, setIsRateLimited] = useState(false)
+  const [notes, setNotes] = useState([])
+  const [isLoading, setIsloading] = useState(true)
+
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/notes')
+      console.log(response.data)
+      setNotes(response.data)
+      setIsRateLimited(false)
+
+    } catch (error) {
+      console.log(error)
+      if (error.response?.status === 429) {
+        setIsRateLimited(true)
+      } else {
+        toast.error('Failed to load notes')
+      }
+
+    } finally {
+      setIsloading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchNotes() 
+  }, [])
+
+  return <div className="min-h-screen">
     <NavBar />
-  </>
+    {isRateLimited && <RateLimited />}
+  </div>
 }
 
 export default Home
